b41fcb97c94559caba3e3e1d3d43e255f539a2ce
Matt Carr <mpcarr84@gmail.com>
2025-08-22 06:59:21 +0100

    code updates

--- mpf/gmc_dmd/slides/bonus-calc.gd
+++ mpf/gmc_dmd/slides/bonus-calc.gd
@@ -4,19 +4,21 @@ extends VBoxContainer
 @onready var lblHeader = $Label
 
 func _ready() -> void:
+	var races = MPF.game.player.races
+	lblBonus.text = "Races " + str(races) + " x " + "100000"
 	MPF.server.add_event_handler("trigger_bonus_combos", self._on_event_combos)
 	MPF.server.add_event_handler("trigger_bonus_nodes", self._on_event_nodes)
 	MPF.server.add_event_handler("trigger_bonus_summary", self._on_event_summary)
 
 func _on_event_combos(payload: Dictionary) -> void:
 	var combos = MPF.game.player.combos
-	lblBonus.text = "Combos" + str(combos) + " x " + "100000"
+	lblBonus.text = "Combos " + str(combos) + " x " + "100000"
 
 func _on_event_nodes(payload: Dictionary) -> void:
 	var nodes = MPF.game.player.nodes_collected
-	lblBonus.text = "Nodes" + str(nodes) + " x " + "50000"
+	lblBonus.text = "Nodes " + str(nodes) + " x " + "50000"
 
 func _on_event_summary(payload: Dictionary) -> void:
 	var bonus_total = MPF.game.player.bonus_total
 	var bonus_multiplier = MPF.game.player.bonus_multiplier
-	lblBonus.text = "Nodes" + str(nodes) + " x " + "50000"
+	lblBonus.text = str(bonus_multiplier) + " x " + str(bonus_total)
